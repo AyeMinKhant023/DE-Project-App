5dd7729c302e8fdb611512044c8b9946b9e938f7
Aye Min Khant <6630613023@psu.ac.th>
2026-04-25 19:12:42 +0700

    Updating app.py (1)

--- app.py
+++ app.py
@@ -4,8 +4,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "<h1>Automated Cloud Security Project</h1>" \
-    "<p>AWS server by Patric is Running!</p>"
+    return "<h1>Welcome to Automated Cloud Security Project</h1>" \
+    "<p>✅ AWS server by Patric is Running!</p>" \
+    "<p>✅ Database connection is successful!</p>" \
 
 if __name__ == '__main__':
     # We use 0.0.0.0 so it listens on the public IP, not just locally
