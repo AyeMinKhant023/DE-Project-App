9f936e6e20e9428e5bec97c16936ac97a684e023
Aye Min Khant <6630613023@psu.ac.th>
2026-04-30 09:43:24 +0700

    Updating test db file to actually create a table and insert data using Parameterized Queries

--- app.py
+++ app.py
@@ -7,7 +7,6 @@ def home():
     return "<h1>Welcome to Automated Cloud Security Project</h1>" \
     "<p>✅ AWS server by Patric is Running!</p>" \
     "<p>✅ Database connection is successful!</p>" \
-    # return "<h1>Oliver said he wanna go to Chiang Mai so bad lol</h1>"
 
 if __name__ == '__main__':
     # We use 0.0.0.0 so it listens on the public IP, not just locally
--- test_db.py
+++ test_db.py
@@ -11,7 +11,21 @@ db_config = {
 try:
     print("Attempting to connect to RDS...")
     conn = mysql.connector.connect(**db_config)
+    cursor = conn.cursor()
     print("✅ SUCCESS! Your EC2 is talking to your RDS Database.")
+
+    # 1. Create a Table (Integrity Test)
+    cursor.execute("CREATE TABLE IF NOT EXISTS students (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255))")
+    
+    # 2. INSERT data using the SECURE Parameterized method
+    student_name = "Patric_Student"
+    sql = "INSERT INTO students (name) VALUES (%s)" # The %s is the 'Shield'
+    cursor.execute(sql, (student_name,)) # We pass the data separately
+
+    conn.commit()
+    print(f"✅ INTEGRITY TEST: Successfully inserted '{student_name}' using Parameterized Query.")
+
+    cursor.close()
     conn.close()
 except Exception as e:
     print("❌ CONNECTION FAILED!")
